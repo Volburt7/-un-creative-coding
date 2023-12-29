f35c8a57f1fe5a96aa3debaf20504263737c9838
Oliver <or-202361@rwu.de>
2023-12-29 21:47:45 +0100

    gears quick save

--- ex_6.2/src/main/java/app/GearSimulation.java
+++ ex_6.2/src/main/java/app/GearSimulation.java
@@ -4,6 +4,8 @@ import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import processing.core.PApplet;
 import processing.core.PShape;
+import processing.event.KeyEvent;
+import processing.event.MouseEvent;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -13,71 +15,89 @@ public class GearSimulation extends PApplet {
 
     final List<Gear> gears = new ArrayList<>();
 
-
-    float ROTATION = 0f;
-
     @Override
     public void settings() {
-        size(1800, 300);
+        size(800, 600);
+    }
+
+    @Override
+    public void mousePressed(final MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() == LEFT) {
+            LOG.info("Pressed left on {} {}", mouseEvent.getX(), mouseEvent.getY());
+        }
+        if (mouseEvent.getButton() == CENTER) {
+            // TODO: Delete this
+            LOG.info("PRESSED Center... not sure how to use this");
+        }
+    }
+
+    @Override
+    public void mouseDragged(final MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() == LEFT) {
+            LOG.info("Moved left on {} {}", mouseEvent.getX(), mouseEvent.getY());
+            // TODO: Show preview here
+        }
+    }
+
+    @Override
+    public void mouseWheel(final MouseEvent mouseEvent) {
+        if (true) { // TODO: If init process of new gear
+            LOG.info("Scrolled {} times.", mouseEvent.getCount());
+        }
+    }
+
+    @Override
+    public void mouseReleased(final MouseEvent mouseEvent) {
+        if (mouseEvent.getButton() == LEFT) {
+            LOG.info("Released left on {} {}", mouseEvent.getX(), mouseEvent.getY());
+        }
+    }
+
+    @Override
+    public void keyPressed(final KeyEvent keyEvent) {
+        LOG.info("Pressed keycode {}", keyEvent.getKeyCode());
     }
 
     public void setup() {
         frameRate(60);
+        exampleGearForTesting();
+    }
+
+    private void exampleGearForTesting() {
+        gears.add(new Gear(100, 100, 2, 0));
+        gears.add(new Gear(300, 100, 3, HALF_PI));
+        gears.add(new Gear(100, 200, 4, PI));
+        gears.add(new Gear(200, 300, 5, 3/TWO_PI));
+        gears.add(new Gear(500, 350, 11, 3/TWO_PI));
     }
 
     public void draw() {
         background(50);
         fill(100);
-        noStroke();
-
-        drawCogwheels(ROTATION);
-        ROTATION += 0.01f;
+        stroke(255);
+        gears.forEach(this::drawGear);
     }
 
 
-    private void drawCogwheels(float rotation) {
+    private void drawGear(final Gear gear) {
         pushMatrix();
-        translate(150, 150);
-        shape(getCogwheelShape(100, 2, rotation));
-        popMatrix();
-
-        pushMatrix();
-        translate(450, 150);
-        shape(getCogwheelShape(100, 3, rotation));
-        popMatrix();
-        pushMatrix();
-        translate(750, 150);
-        shape(getCogwheelShape(100, 4, rotation));
-        popMatrix();
-
-        pushMatrix();
-        translate(1050, 150);
-        shape(getCogwheelShape(100, 5, rotation));
-        popMatrix();
-
-        pushMatrix();
-        translate(1350, 150);
-        shape(getCogwheelShape(100, 10, rotation));
-        popMatrix();
-
-        pushMatrix();
-        translate(1650, 150);
-        shape(getCogwheelShape(100, 1, rotation));
+        translate(gear.getPositionX(), gear.getPositionY());
+        shape(getGearShape(gear));
         popMatrix();
     }
 
-    private PShape getCogwheelShape(final float outerRadius, final int cogNum, final float rotation) {
-        final PShape cogWheelShape = createShape();
+    private PShape getGearShape(final Gear gear) {
+        final PShape gearShape = createShape();
 
-        final PShape outerCircle = getOuterCircle(outerRadius);
-        final List<PShape> cogs = getCogs(outerRadius, cogNum, rotation);
+        final List<PShape> cogs = getTeeth(gear);
+        final PShape outerCircle = getOuterCircle(gear.getRadius());
 
-        cogWheelShape.beginShape();
-        cogWheelShape.addChild(outerCircle);
-        cogs.forEach(cogWheelShape::addChild);
-        cogWheelShape.endShape(CLOSE);
+        gearShape.beginShape();
+        cogs.forEach(gearShape::addChild);
+        gearShape.addChild(outerCircle);
+        gearShape.endShape(CLOSE);
 
-        return cogWheelShape;
+        return gearShape;
     }
 
     private PShape getOuterCircle(float outerRadius) {
@@ -90,33 +110,31 @@ public class GearSimulation extends PApplet {
         ellipse(0, 0, outerRadius, outerRadius);
         outerCircle.beginContour();
         fill(g.backgroundColor);
-        ellipse(0, 0, outerRadius/3, outerRadius/3);
+        ellipse(0, 0, outerRadius/2, outerRadius/2);
         outerCircle.endContour();
         outerCircle.endShape(CLOSE);
         return outerCircle;
     }
 
-    private List<PShape> getCogs(float outerRadius, int cogNum, float rotation) {
-        final List<PShape> cogs = new ArrayList<>();
-        final float cogDegree = TWO_PI / cogNum;
+    private List<PShape> getTeeth(final Gear gear) {
+        final List<PShape> teeth = new ArrayList<>();
+        final float degreePerTeeth = TWO_PI / gear.getTeethCount();
 
         fill(100);
         rectMode(RADIUS);
 
-        for (int i = 1; i <= cogNum; i++) {
-            final PShape cog = createShape();
-            cog.beginShape();
-            final float curRotation = cogDegree * i + rotation;
-            final float x = outerRadius * cos(curRotation);
-            final float y = outerRadius * sin(curRotation);
+        for (int i = 1; i <= gear.getTeethCount(); i++) {
+            final PShape tooth = createShape();
+            tooth.beginShape();
+            final float curRotation = degreePerTeeth * i + gear.getCurrentRotation();
             pushMatrix();
-            translate(x, y);
+            translate(gear.getRadius() * cos(curRotation), gear.getRadius() * sin(curRotation));
             rotate(curRotation);
-            rect(0, 0, outerRadius/5, outerRadius/5);
+            rect(0, 0, gear.getToothSize(), gear.getToothSize() - 3);
             popMatrix();
-            cog.endShape();
-            cogs.add(cog);
+            tooth.endShape();
+            teeth.add(tooth);
         }
-        return cogs;
+        return teeth;
     }
 }
--- ex_6.2/src/main/java/app/obj/Motor.java
+++ ex_6.2/src/main/java/app/obj/Motor.java
@@ -1,5 +1,14 @@
 package app.obj;
 
-public class Motor extends Gear {
+import lombok.Getter;
+import lombok.Setter;
 
+@Getter
+@Setter
+public class Motor extends Gear {
+    private float rotationSpeed;
+
+    public Motor(int positionX, int positionY, int teethCount, float currentRotation) {
+        super(positionX, positionY, teethCount, currentRotation);
+    }
 }
