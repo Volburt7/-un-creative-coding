2cd8a92e6a32b027313db63fad974e4c5aed7a05
Oliver <or-202361@rwu.de>
2024-01-15 15:06:51 +0100

    make hello world faster

--- ex_1.1/src/main/java/app/HelloWorld.java
+++ ex_1.1/src/main/java/app/HelloWorld.java
@@ -2,9 +2,13 @@ package app;
 
 import processing.core.PApplet;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class HelloWorld extends PApplet {
 
-    private int nextCheck = 5;
+    private final int CHECKS = 20;
+    private List<Integer> nextCheckList = new ArrayList<>();
 
     public void settings() {
         size(512, 512);
@@ -13,17 +17,34 @@ public class HelloWorld extends PApplet {
     @Override
     public void setup() {
         background(45, 45, 70);
+        setCheckList();
+    }
+
+    private void setCheckList() {
+        for (int i = 0; i < CHECKS; i++) {
+            nextCheckList.add(getCheck());
+        }
+    }
+
+    private int getCheck() {
+        return (int) random(10, 200);
     }
 
     @Override
     public void draw() {
         frameRate(60);
-        if (frameCount % nextCheck == 0) {
-            noStroke();
-            fill(random(0, 100), random(100, 180), random(80, 160));
-            float radius = random(12, 30);
-            ellipse(random(0, width), random(0, height), radius, radius);
-            nextCheck = (int) random(5, 30);
-        }
+        new ArrayList<>(nextCheckList).forEach(check -> {
+            if (frameCount % check == 0) {
+                noStroke();
+                fill(random(0, 100), random(100, 180), random(80, 160));
+                float radius = random(12, 30);
+                ellipse(random(0, width), random(0, height), radius, radius);
+
+                nextCheckList.remove(check);
+                nextCheckList.add(getCheck());
+            }
+        });
     }
+
+
 }
