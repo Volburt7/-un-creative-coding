d11fffc47cee71dc7994054397eb35d5f86d854c
Oliver <or-202361@rwu.de>
2024-04-18 01:55:36 +0200

    Update own_3: add comment

--- own_3_bouncy_spheres/src/main/java/app/BouncingBalls.java
+++ own_3_bouncy_spheres/src/main/java/app/BouncingBalls.java
@@ -8,6 +8,10 @@ import processing.core.PVector;
 import java.util.ArrayList;
 import java.util.List;
 
+/*
+* Idee von https://www.reddit.com/r/oddlysatisfying/comments/19bgdvr/the_chance_of_probability/?share_id=XF-G2q8ryQhpwprF2WaFH&utm_content=1&utm_medium=android_app&utm_name=androidcss&utm_source=share&utm_term=3
+* Partikelsystem von hier kopiert: https://processing.org/examples/smokeparticlesystem.html
+* */
 public class BouncingBalls extends PApplet {
     private static final Logger LOG = LoggerFactory.getLogger(BouncingBalls.class);
 
