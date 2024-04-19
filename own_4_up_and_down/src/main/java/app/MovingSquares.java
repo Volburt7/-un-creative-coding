9fcfbc017461b6d827da9b41eb9bf04d5494314b
Oliver <or-202361@rwu.de>
2024-04-19 02:40:22 +0200

    Update own_4: looks weird

--- own_4_up_and_down/src/main/java/app/MovingSquares.java
+++ own_4_up_and_down/src/main/java/app/MovingSquares.java
@@ -6,6 +6,7 @@ import processing.core.PApplet;
 import processing.core.PVector;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 
 /*
@@ -17,8 +18,9 @@ public class MovingSquares extends PApplet {
 
     // size must be dividable by square size
     private static final int SIZE = 512;
-    // If square_size changes generatePixels() also needs an update
     private static final int SQUARE_SIZE = 64;
+    private static final int IT_STEP_SIZE = 8;
+
     private final int bgColor = color(145, 39, 27);
     private List<Square> squares = new ArrayList<>();
 
@@ -30,21 +32,25 @@ public class MovingSquares extends PApplet {
     @Override
     public void setup() {
         frameRate(60);
-        initializeSquares();
+        this.squares = createSquares();
     }
 
-    private void initializeSquares() {
+    private List<Square> createSquares() {
         final int max = SIZE / SQUARE_SIZE;
-        final List<Square> squares = new ArrayList<>();
+        final List<Square> squareList = new ArrayList<>();
         // Iteration with 2 for loops, so we can skip row/col 1 and last
         for (int i = 0; i < max; i++) {
-            if (i == 0 || i == max - 1) {continue;}
+            if (i == 0 || i == max - 1) {
+                continue;
+            }
             for (int j = 0; j < max; j++) {
-                if (j == 0 || j == max - 1) {continue;}
-                squares.add(createSquare(i, j));
+                if (j == 0 || j == max - 1) {
+                    continue;
+                }
+                squareList.add(createSquare(i, j));
             }
         }
-        this.squares = squares;
+        return squareList;
     }
 
     private Square createSquare(final int i, final int j) {
@@ -57,19 +63,16 @@ public class MovingSquares extends PApplet {
 
     private int[][] generatePixels() {
         final int[][] pixels = new int[SQUARE_SIZE][SQUARE_SIZE];
-        final int steps = 8;
-        final int start = 0;
-        final int end = SQUARE_SIZE - start;
-        for (int i = start; i < end; i += steps) {
-            for (int j = start; j < end; j += steps) {
+        for (int i = 0; i < SQUARE_SIZE; i += IT_STEP_SIZE) {
+            for (int j = 0; j < SQUARE_SIZE; j += IT_STEP_SIZE) {
                 // choose a random color
                 int color = color(0);
                 if (random(0, 1) > 0.5f) {
                     color = color(bgColor);
                 }
                 // This sets the color values for steps in x and y direction
-                for (int innerI = i; innerI < i + steps; innerI++) {
-                    for (int innerJ = j; innerJ < j + steps; innerJ++) {
+                for (int innerI = i; innerI < i + IT_STEP_SIZE; innerI++) {
+                    for (int innerJ = j; innerJ < j + IT_STEP_SIZE; innerJ++) {
                         pixels[innerI][innerJ] = color;
                     }
                 }
@@ -83,14 +86,15 @@ public class MovingSquares extends PApplet {
         background(bgColor);
         squares.forEach(this::drawSquare);
         squares.forEach(this::drawBordersBetween);
-        squares.forEach(this::updateSquare);
+        squares.forEach(this::updatePixelPositions);
+        squares.forEach(this::colorizeTop);
     }
 
     private void drawSquare(final Square square) {
         int[][] squarePixelsArray = getPixelsSwapColAndRow(square);
-        for (int i = 0; i < squarePixelsArray.length; i++) {
+        for (int i = 0; i < SQUARE_SIZE; i++) {
             int[] squarePixels = squarePixelsArray[i];
-            for (int j = 0; j < squarePixels.length; j++) {
+            for (int j = 0; j < SQUARE_SIZE; j++) {
                 int pixelColor = squarePixels[j];
                 pushMatrix();
                 stroke(pixelColor);
@@ -100,10 +104,11 @@ public class MovingSquares extends PApplet {
             }
         }
     }
+
     private void drawBordersBetween(final Square square) {
         pushMatrix();
-        fill(0,0,0,0);
-        stroke(bgColor,255);
+        fill(0, 0, 0, 0);
+        stroke(bgColor, 255);
         strokeWeight(5f);
         rect(square.getPosition().x, square.getPosition().y, SQUARE_SIZE, SQUARE_SIZE);
         popMatrix();
@@ -111,31 +116,27 @@ public class MovingSquares extends PApplet {
 
     private int[][] getPixelsSwapColAndRow(final Square square) {
         int[][] squarePixelsArray = square.getPixels().clone();
-        if (squarePixelsArray.length <= 0) {
-            LOG.warn("squarePixelsArray empty");
-            return squarePixelsArray;
-        }
-        int[][] swapped = new int[squarePixelsArray[0].length][squarePixelsArray.length];
-        for (int i = 0; i < squarePixelsArray.length; i++) {
-            for (int j = 0; j < squarePixelsArray[i].length; j++) {
+        int[][] swapped = new int[SQUARE_SIZE][SQUARE_SIZE];
+        for (int i = 0; i < SQUARE_SIZE; i++) {
+            for (int j = 0; j < SQUARE_SIZE; j++) {
                 swapped[j][i] = squarePixelsArray[i][j];
             }
         }
         return swapped;
     }
 
-    private void updateSquare(final Square square) {
+    private void updatePixelPositions(final Square square) {
         int[][] squarePixelsArray = square.getPixels().clone();
-        for (int i = 0; i < squarePixelsArray.length; i++) {
+        for (int i = 0; i < SQUARE_SIZE; i++) {
             final int[] line = square.getPixels()[i];
             if (square.isGoUp()) {
                 if (i == 0) {
-                    squarePixelsArray[squarePixelsArray.length - 1] = line;
+                    squarePixelsArray[SQUARE_SIZE - 1] = line;
                 } else {
                     squarePixelsArray[i - 1] = line;
                 }
             } else {
-                if (i == squarePixelsArray.length - 1) {
+                if (i == SQUARE_SIZE - 1) {
                     squarePixelsArray[0] = line;
                 } else {
                     squarePixelsArray[i + 1] = line;
@@ -143,5 +144,35 @@ public class MovingSquares extends PApplet {
             }
         }
         square.setPixels(squarePixelsArray);
+    }
+
+    private void colorizeTop(final Square square) {
+        boolean[] bgPreviously = new boolean[SQUARE_SIZE];
+        Arrays.fill(bgPreviously, true);
+//        final int limit = (int) IT_STEP_SIZE / 2;
+        final int limit = 1;
+        resetColorizedTop(square);
+        for (int i = 0; i < SQUARE_SIZE; i+=2*IT_STEP_SIZE) {
+            for (int j = 0; j < SQUARE_SIZE; j++) {
+                if (bgPreviously[j] && square.getPixels()[i][j] == color(0)) {
+                    for (int k = 0; k < limit; k++) {
+                        square.getPixels()[i + k][j] = color(200, 135, 30);
+                        bgPreviously[j] = false;
+                    }
+                } else if (square.getPixels()[i][j] == color(bgColor)) {
+                    bgPreviously[j] = true;
+                }
+            }
+        }
+    }
+
+    private void resetColorizedTop(final Square square) {
+        for (int i = 0; i < SQUARE_SIZE; i += IT_STEP_SIZE) {
+            for (int j = 0; j < SQUARE_SIZE; j++) {
+                if (square.getPixels()[i][j] == color(200, 135, 30)) {
+                    square.getPixels()[i][j] = color(0);
+                }
+            }
+        }
     }
 }
