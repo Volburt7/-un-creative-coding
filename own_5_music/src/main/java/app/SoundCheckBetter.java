f2ee7dce4cbcedb3f8539dd3b1bf21c18540e70d
Oliver <or-202361@rwu.de>
2024-04-19 14:06:46 +0200

    alternative to play mp3 3rd party

--- own_5_music/src/main/java/app/SoundCheckBetter.java
+++ own_5_music/src/main/java/app/SoundCheckBetter.java
@@ -1,19 +1,22 @@
 package app;
 
+import javazoom.jl.decoder.JavaLayerException;
+import javazoom.jl.player.Player;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import processing.core.PApplet;
 
-import javax.sound.sampled.LineUnavailableException;
-import javax.sound.sampled.UnsupportedAudioFileException;
-import java.io.IOException;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.concurrent.CompletableFuture;
 
 public class SoundCheckBetter extends PApplet {
     private static final Logger LOG = LoggerFactory.getLogger(SoundCheckBetter.class);
 
-    private final List<MySoundPlayer> soundPlayers = new ArrayList<>();
+    private final List<Player> soundPlayers = new ArrayList<>();
     private int currentSoundIndex = 0;
 
     @Override
@@ -24,12 +27,13 @@ public class SoundCheckBetter extends PApplet {
     @Override
     public void setup() {
         frameRate(60);
-//        for (int i = 1; i <= 24; i++) {
-        for (int i = 1; i <= 1; i++) {
-            final String fileName = "key" + nf(i, 2) + ".MP3";
+        for (int i = 1; i <= 24; i++) {
+            final String fileName = "key" + nf(i, 2) + ".mp3";
             try {
-                soundPlayers.add(new MySoundPlayer(fileName));
-            } catch (UnsupportedAudioFileException | LineUnavailableException | IOException e) {
+                final File file = new File(getClass().getClassLoader().getResource(fileName).getFile());
+                final FileInputStream fis = new FileInputStream(file);
+                soundPlayers.add(new Player(fis));
+            } catch (FileNotFoundException | JavaLayerException e) {
                 LOG.error("Could not instantiate Soundplayer for file: '{}'", fileName, e);
                 exit();
             }
@@ -45,13 +49,29 @@ public class SoundCheckBetter extends PApplet {
         background(0);
         fill(255);
         if (frameCount % 60 == 0) {
-            playSound();
+            try {
+                playSound();
+            } catch (JavaLayerException e) {
+                LOG.error("Could not play sound", e);
+            }
         }
     }
 
-    private void playSound() {
-        soundPlayers.get(currentSoundIndex).play();
+    private void playSound() throws JavaLayerException {
+        updateSoundIndex();
+        final int copy = currentSoundIndex;
+        CompletableFuture.runAsync(() -> {
+            LOG.info("Playing soundfile at index '{}'", currentSoundIndex);
+            try {
+                soundPlayers.get(copy).play();
+                LOG.info("Played sound at index '{}'", currentSoundIndex);
+            } catch (JavaLayerException e) {
+                throw new RuntimeException(e);
+            }
+        });
+    }
 
+    private void updateSoundIndex() {
         currentSoundIndex += 1;
         if (soundPlayers.size() <= currentSoundIndex) {
             currentSoundIndex = 0;
