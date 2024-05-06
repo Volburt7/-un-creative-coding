0db9d7455521cc3c1eeb1ebf3b110295d29e69b6
Oliver <or-202361@rwu.de>
2024-05-06 13:08:28 +0200

    ex2_4: fix

--- ex24_3d_circles/src/main/java/app/MovingCircles.java
+++ ex24_3d_circles/src/main/java/app/MovingCircles.java
@@ -4,9 +4,9 @@ import processing.core.PApplet;
 
 public class MovingCircles extends PApplet {
 
-    private final static int SQUARE_SIZE = 32;
-    private final static int BIG_CIRCLE_SIZE = SQUARE_SIZE - 1;
-    private final static int SMALL_CIRCLE_SIZE = SQUARE_SIZE / 2;
+    private static final int SQUARE_SIZE = 32;
+    private static final int BIG_CIRCLE_SIZE = SQUARE_SIZE - 1;
+    private static final int SMALL_CIRCLE_SIZE = SQUARE_SIZE / 2;
     private Float[][] randoms;
 
     public void settings() {
@@ -32,21 +32,31 @@ public class MovingCircles extends PApplet {
         for (int w = 0; w < width; w += SQUARE_SIZE) {
             for (int h = 0; h < height; h += SQUARE_SIZE) {
                 drawBigCircles(w, h);
+//                For the 3D effect use this instead
+//                drawSmallCircles(w, h);
+            }
+        }
+        for (int w = 0; w < width; w += SQUARE_SIZE) {
+            for (int h = 0; h < height; h += SQUARE_SIZE) {
                 drawSmallCircles(w, h);
             }
         }
     }
 
     private void drawBigCircles(final int w, final int h) {
+        pushMatrix();
         fill(0);
         final Float random = randoms[w / SQUARE_SIZE][h / SQUARE_SIZE];
-        final float mappedX = map(mouseX, 0, width, -SQUARE_SIZE * 4, SQUARE_SIZE * 4) * random;
-        final float mappedY = map(mouseY, 0, height, -SQUARE_SIZE * 4, SQUARE_SIZE * 4) * -random;
+        final float mappedX = map(mouseX, 0, width, -SQUARE_SIZE * 4f, SQUARE_SIZE * 4f) * random;
+        final float mappedY = map(mouseY, 0, height, -SQUARE_SIZE * 4f, SQUARE_SIZE * 4f) * -random;
         ellipse(mappedX + w + (float) SQUARE_SIZE / 2, mappedY + h + (float) SQUARE_SIZE / 2, BIG_CIRCLE_SIZE, BIG_CIRCLE_SIZE);
+        popMatrix();
     }
 
     private void drawSmallCircles(final int w, final int h) {
+        pushMatrix();
         fill(255);
         ellipse(w + (float) SQUARE_SIZE / 2, h + (float) SQUARE_SIZE / 2, SMALL_CIRCLE_SIZE, SMALL_CIRCLE_SIZE);
+        popMatrix();
     }
 }
